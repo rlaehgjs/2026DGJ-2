ab2abd0ad50c208cb9dba81b35f0bbbd56adf570
Cuyacuya <59507274+Cuyacuya@users.noreply.github.com>
2026-07-20 16:12:30 +0900

    Test: #24 냉장고 레이캐스트 진행 검증

--- Assets/Editor/InteractionSandboxSetupTests.cs
+++ Assets/Editor/InteractionSandboxSetupTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -48,6 +49,44 @@ public class InteractionSandboxSetupTests
         }
     }
 
+    [Test]
+    public void InteractionSandbox_RefrigeratorRaycast_AdvancesMission()
+    {
+        Scene sandboxScene = EditorSceneManager.OpenScene(ScenePath, OpenSceneMode.Additive);
+
+        try
+        {
+            PlayerInteraction playerInteraction = FindComponent<PlayerInteraction>(sandboxScene);
+            PlayerInventory playerInventory = FindComponent<PlayerInventory>(sandboxScene);
+            GameProgressManager progressManager = FindComponent<GameProgressManager>(sandboxScene);
+            Camera interactionCamera = playerInteraction.GetComponentInChildren<Camera>();
+
+            Assert.That(playerInteraction, Is.Not.Null);
+            Assert.That(playerInventory, Is.Not.Null);
+            Assert.That(progressManager, Is.Not.Null);
+            Assert.That(interactionCamera, Is.Not.Null);
+
+            playerInteraction.transform.position = new Vector3(0f, playerInteraction.transform.position.y, 2f);
+            SetPlayerInteractionReferences(playerInteraction, interactionCamera, playerInventory);
+            progressManager.RestoreState(GameProgressState.FindKitchen);
+            Assert.That(progressManager.TryCompleteKitchenArrival(), Is.True);
+            Physics.SyncTransforms();
+
+            MethodInfo tryInteractMethod = typeof(PlayerInteraction).GetMethod(
+                "TryInteract",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+            Assert.That(tryInteractMethod, Is.Not.Null);
+            tryInteractMethod.Invoke(playerInteraction, null);
+
+            Assert.That(progressManager.CurrentState, Is.EqualTo(GameProgressState.FindFrontDoorKey));
+        }
+        finally
+        {
+            EditorSceneManager.CloseScene(sandboxScene, true);
+        }
+    }
+
     private static T FindComponent<T>(Scene scene) where T : Component
     {
         foreach (GameObject rootObject in scene.GetRootGameObjects())
@@ -66,5 +105,22 @@ public class InteractionSandboxSetupTests
     private static Object GetObjectReference(Object component, string propertyName)
     {
         return new SerializedObject(component).FindProperty(propertyName).objectReferenceValue;
+    }
+
+    private static void SetPlayerInteractionReferences(
+        PlayerInteraction playerInteraction,
+        Camera interactionCamera,
+        PlayerInventory playerInventory)
+    {
+        SerializedObject serializedInteraction = new SerializedObject(playerInteraction);
+        serializedInteraction.FindProperty("interactionCamera").objectReferenceValue = interactionCamera;
+        serializedInteraction.ApplyModifiedPropertiesWithoutUndo();
+
+        FieldInfo inventoryField = typeof(PlayerInteraction).GetField(
+            "playerInventory",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+
+        Assert.That(inventoryField, Is.Not.Null);
+        inventoryField.SetValue(playerInteraction, playerInventory);
     }
 }
